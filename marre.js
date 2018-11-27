65f84276a296e778ef2b9b90825af81703becc7b
pmarre <pmarre40@gmail.com>
2018-11-27 08:14:55 -0800

    Add Menu Scroll Function

--- marre.js
+++ marre.js
@@ -5,3 +5,23 @@ function openNav() {
 function closeNav() {
     document.getElementById('myNav').style.height = "0%";
 };
+
+
+const home = $('#banner').offset().top;
+const about = $('#about').offset().top;
+const port = $('#portfolio').offset().top;
+const education = $('#education').offset().top;
+
+$(document).scroll(function() {
+    var scroll = $(document).scrollTop();
+    if (scroll >= home && scroll < about) {
+        $('.nav-item').css('border', 'none');
+        $('.nav1').css('border-bottom', '3px coral solid');
+    } else if (scroll >= port && scroll < education) {
+        $('.nav-item').css('border', 'none');
+         $('.nav2').css('border-bottom', '3px coral solid');
+    } else if (scroll >= education) {
+        $('.nav-item').css('border', 'none');
+         $('.nav3').css('border-bottom', '3px coral solid');
+    }
+});
